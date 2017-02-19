7a8795706d2441c2040361ab1cd75030c381a916
Arnaud Dollois <arnaud@dollois.com>
2017-02-19 17:48:57 +0100

    Fixture Age entity

--- src/AppBundle/DataFixtures/ORM/LoadAgeData.php
+++ src/AppBundle/DataFixtures/ORM/LoadAgeData.php
@@ -1,15 +1,86 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ado
- * Date: 19/02/17
- * Time: 17:06
- */
-
 namespace AppBundle\DataFixtures\ORM;
 
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use AppBundle\Entity\Age;
+use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadAgeData
+
+class LoadAgeData implements FixtureInterface
 {
+    public function load(ObjectManager $manager)
+    {
+        $age = new Age();
+        $age->setName('Poussinets')
+            ->setMin(6)
+            ->setMax(6);
+
+        $manager->persist($age);
+        $manager->flush();
+
+        $age = new Age();
+        $age->setName('Mini-Poussins')
+            ->setMin(7)
+            ->setMax(8);
+
+        $manager->persist($age);
+        $manager->flush();
+
+        $age = new Age();
+        $age->setName('Poussins')
+            ->setMin(9)
+            ->setMax(10);
+
+        $manager->persist($age);
+        $manager->flush();
+
+        $age = new Age();
+        $age->setName('Benjamins')
+            ->setMin(11)
+            ->setMax(12);
+
+        $manager->persist($age);
+        $manager->flush();
+
+        $age = new Age();
+        $age->setName('Minimes')
+            ->setMin(13)
+            ->setMax(14);
+
+        $manager->persist($age);
+        $manager->flush();
+
+        $age = new Age();
+        $age->setName('Cadets')
+            ->setMin(15)
+            ->setMax(17);
+
+        $manager->persist($age);
+        $manager->flush();
+
+        $age = new Age();
+        $age->setName('Juniors')
+            ->setMin(18)
+            ->setMax(20);
+
+        $manager->persist($age);
+        $manager->flush();
+
+        $age = new Age();
+        $age->setName('Seniors')
+            ->setMin(21)
+            ->setMax(39);
+
+        $manager->persist($age);
+        $manager->flush();
+
+        $age = new Age();
+        $age->setName('Vétéran')
+            ->setMin(40)
+            ->setMax(100);
+
+        $manager->persist($age);
+        $manager->flush();
+    }
 
 }
