8bc09651d19ba82dd983c3f51289046b0193b5b8
Arnaud Dollois <arnaud@dollois.com>
2017-03-04 14:27:16 +0100

    WIP

--- src/AppBundle/Controller/MembersController.php
+++ src/AppBundle/Controller/MembersController.php
@@ -1,11 +1,22 @@
 <?php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Member;
+use AppBundle\Form\MemberType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MembersController extends Controller
 {
     public function listAction(){
         return $this->render('members/list.html.twig');
+    }
+
+    public function addAction(){
+        $member = new Member();
+        $formMember = $this->get('form.factory')->create(MemberType::class, $member);
+
+        return $this->render('members/add.html.twig', array(
+            'form' => $formMember->createView(),
+        ));
     }
 }
---
+++ src/AppBundle/Form/MemberType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+class MemberType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('birthday', DateType::class)
+            ->add('avatar', FileType::class)
+            ->add('sex', EntityType::class, array('class' => 'AppBundle\Entity\Sex', 'multiple' => false, 'expanded' => true, 'choice_label' => 'lib' ))
+            ->add('save', SubmitType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'AppBundle\Entity\Member',
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_member';
+    }
+
+
+}
