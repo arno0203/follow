7d09f388d75d0190f4c2625ef8de10239a420331
Arnaud Dollois <arnaud@dollois.com>
2017-04-30 11:06:53 +0200

    Automatic logout user after period of inactivity

---
+++ src/AppBundle/Handler/SessionIdleHandler.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Handler;
+
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class SessionIdleHandler
+{
+    protected $session;
+    protected $securityToken;
+    protected $router;
+    protected $maxIdleTime;
+
+    /**
+     * SessionIdleHandler constructor.
+     * @param SessionInterface $session
+     * @param TokenStorageInterface $securityToken
+     * @param RouterInterface $router
+     * @param int $maxIdleTime
+     */
+    public function __construct(SessionInterface $session, TokenStorageInterface $securityToken, RouterInterface $router, $maxIdleTime = 0)
+    {
+        $this->session = $session;
+        $this->securityToken = $securityToken;
+        $this->router = $router;
+        $this->maxIdleTime = $maxIdleTime;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST != $event->getRequestType()) {
+            return;
+        }
+
+        if ($this->maxIdleTime > 0) {
+
+            $this->session->start();
+            $lapse = time() - $this->session->getMetadataBag()->getLastUsed();
+
+            if ($lapse > $this->maxIdleTime) {
+
+                $this->securityToken->setToken(null);
+                $this->session->getFlashBag()->set('info', 'You have been logged out due to inactivity.');
+
+                // Change the route if you are not using FOSUserBundle.
+                $event->setResponse(new RedirectResponse($this->router->generate('fos_user_security_login')));
+            }
+        }
+    }
+}
